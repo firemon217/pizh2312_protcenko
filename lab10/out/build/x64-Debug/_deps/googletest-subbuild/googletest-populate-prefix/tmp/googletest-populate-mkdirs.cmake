99e754a93f3cc773e46065e56b8d32e6e1667f2e
Dmitry <pilot7736gives@gmail.com>
2025-04-29 22:09:37 +0300

    add lab 10

---
+++ lab10/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/tmp/googletest-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-src"
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-build"
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix"
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/tmp"
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp"
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/src"
+  "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Work/University/OOP/pizh2312_protcenko/lab10/ArgParser/out/build/x64-Debug/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
